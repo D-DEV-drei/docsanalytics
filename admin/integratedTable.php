bf5e8eff6b8855d288c11dce731229b71656d8d1
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-04-10 17:21:00 +0800

    integratedTable structure adjusted

--- admin/integratedTable.php
+++ admin/integratedTable.php
@@ -252,43 +252,8 @@ $folder = $_GET['folder'] ?? '';
             ?>
 
             <br>
-            <br>
-            <br>
         </main>
     </div>
-
-
-
-<!-- Rename Folder Modal -->
-    <div class="modal fade" id="renameFolderModal" tabindex="-1" role="dialog" aria-labelledby="renameFolderModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="renameFolderModalLabel">Rename Folder</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form id="renameFolderForm">
-                        <div class="form-group">
-                            <label for="newFolderName">New Folder Name:</label>
-                            <input type="text" class="form-control" id="newFolderName" name="newFolderName" required>
-                        </div>
-                        <input type="hidden" id="folderIdToUpdate" name="folderIdToUpdate">
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" onclick="renameFolder()">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Include addFolderModal.php which is modal for adding new user-->
-    <?php include '../modal/addFolderModal.php'?>
-    <?php include '../modal/restoreFolderModal.php'?>
 
     <!-- Include your jQuery and other JavaScript files here -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
